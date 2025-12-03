21c3f995ed9e3335a6e8ff270bd2d2ded3782a31
Christophe LC <christophelecoent@gmail.com>
2025-12-03 21:26:17 +0000

    fix error in : /home/user/studio/src/app/mutations/autoriser-modificatio

--- src/app/mutations/autoriser-modification-des-droits/event.ts
+++ src/app/mutations/autoriser-modification-des-droits/event.ts
@@ -4,7 +4,7 @@ import type { BaseEvent } from '../mutation-lifecycle/cqrs';
 
 // Event
 export interface ModificationDroitsAutoriseeEvent extends BaseEvent {
-    type: 'MODIFICATION_RESSOURCES_AUTORISEE';
+    type: 'MODIFICATION_DROITS_AUTORISEE';
     payload: {
         userEmail: string;
     }
